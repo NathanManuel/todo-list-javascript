c75f8edec76930fb068599be4ab13a4625651cb9
Nathan van Velden <nathanvanvelden@gmail.com>
2022-12-07 15:57:57 +0200

    removed index.js added script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+var btn = document.getElementById("btn");
+var input = document.getElementById("text-input");
+var list = document.getElementById("to-do-list");
+var deleteI = document.getElementById("deleteI");
+var nr = document.getElementById("number-todo");
+
+var todos = 0;
+btn.onclick = () => {
+  if (input.value) {
+    updateList(input.value);
+    todos++;
+    nr.innerHTML = `${todos} Items`;
+  }
+};
+
+function deleteItem(del) {
+  console.log(del.parentElement);
+  del.parentElement.remove();
+  todos--;
+  nr.innerHTML = `${todos} Items`;
+}
+
+function clearAll() {
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  todos = 0;
+  nr.innerHTML = `${todos} Items`;
+}
+
+function checkB(check) {
+  check.classList.toggle("checkbox-round-select-checked");
+}
+
+function updateList(text) {
+  var html = "";
+  html += `<div id="todoI">
+  <input type="checkbox" onclick="checkB(this)" class="checkbox-round checkbox-round-select" />
+    <a class="todo-text">${text}</a>
+    <button onclick="deleteItem(this)" class="checkbox-round checkbox-round-delete">X</button></div>`;
+  list.innerHTML += html;
+}
+
+nr.innerHTML = `${todos} Items`;
